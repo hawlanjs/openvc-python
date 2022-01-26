7a3704a927705f2ba171bd6c9fddea26d8c79cda
hawlan <83552463+hawlanjs@users.noreply.github.com>
2022-01-26 14:53:08 +0100

    Create reading-image.py

---
+++ reading-image.py
@@ -0,0 +1,6 @@
+import cv2 #importing a pakege
+
+img = cv2.imread("lena.png") #reading a image. and you can change a lena.png to wat you want.
+
+cv2.imshow("imine",img) #showing a image 
+cv2.waitKey(0) #delay
